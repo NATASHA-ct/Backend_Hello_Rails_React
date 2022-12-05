4f5a92f5e32723159ba70febfa986ce17b90676f
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-12-05 18:28:23 +0200

    Add cors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::GreetingsController < ApplicationController
   # GET /greetings or /greetings.json
   def index
-    @greetings = Greeting.all
-    render json: @greetings
-  end
-
-  def show
     @greetings = Greeting.all.sample
     render json: @greetings
   end
+
+  # def show
+  #   @greetings = Greeting.all.sample
+  #   render json: @greetings
+  # end
 end
